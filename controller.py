631cb76dbb74cd714bfc45e92a2c40b12c367a78
Jtanner5674 <79053412+Jtanner5674@users.noreply.github.com>
2025-02-26 12:57:14 -0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -7,6 +7,12 @@ def index():
     """ Main page route """
     return render_template('index.html')
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+@app.route('/.well-known/pki-validation/7808F76644558602520F32FD9DF9790C.txt')
+def serve_pki_validation():
+    return send_from_directory(BASE_DIR, "7808F76644558602520F32FD9DF9790C.txt")
+
 # Flask app initialization
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
